33e028ae6a09905d48d543a669b4a7baf51d59dd
Shwet Kashyap <91766101+SHWETKASHYAP@users.noreply.github.com>
2026-01-18 08:53:55 +0530

    Remove CORS middleware from server.js
    Removed CORS middleware configuration from server.js.

--- server.js
+++ server.js
@@ -14,17 +14,6 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const PORT = process.env.PORT || 10000;
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://mentormind-delta.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: false, // IMPORTANT
-  })
-);
 
 mongoose
   .connect(process.env.MONGO_URL)
