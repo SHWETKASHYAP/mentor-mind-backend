68588a5195ccb93e5547636a0245b179a2e803c0
Shwet Kashyap <91766101+SHWETKASHYAP@users.noreply.github.com>
2026-01-18 09:32:46 +0530

    Modify signup to require only email and password
    Updated signup validation to require only email and password.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,7 @@ export const signup = async(req,res) => {
     try{
         const {name, email, password} = req.body;
 
-        if(!name || !email || !password){
+        if(!email || !password){
             return res.status(400).json({message: "All fields required"});
         }
 
